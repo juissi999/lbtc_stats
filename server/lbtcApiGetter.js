f0a2e12395877fd5f3060e7864236c6e4892ec43
juissi999 <jussi.kaatiala@gmail.com>
2020-05-19 21:01:22 +0300

    lbtcApi tuned and baseurl moved to parameter

--- server/lbtcApiGetter.js
+++ server/lbtcApiGetter.js
@@ -1,16 +1,12 @@
 const axios = require('axios')
+const baseUrl = 'https://localbitcoins.com'
 
 exports.getRecent = () => {
-  const request = axios.get(
-    'https://localbitcoins.com/bitcoincharts/eur/trades.json'
-  )
+  const request = axios.get(`${baseUrl}/bitcoincharts/eur/trades.json`)
   return request.then((response) => response.data)
 }
 
 exports.getSell = () => {
-  const request = axios.get(
-    'https://localbitcoins.com/sell-bitcoins-online/eur/.json',
-    { headers: { 'Content-Type': 'application/json' } }
-  )
+  const request = axios.get(`${baseUrl}/sell-bitcoins-online/eur/.json`)
   return request.then((response) => response.data)
 }
