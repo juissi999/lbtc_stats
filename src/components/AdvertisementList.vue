eb2ec38697bbaffb03aa1133864fcc6bfcaa447c
juissi999 <jussi.kaatiala@gmail.com>
2020-05-21 18:33:01 +0300

    Fixed v-bind key and added comments

--- src/components/AdvertisementList.vue
+++ src/components/AdvertisementList.vue
@@ -1,18 +1,22 @@
 <template>
   <ul>
     <li
-      v-bind:key="i"
-      v-for="(li, i) in tlist.filter((d, i) => i < transactionCount)"
+      v-bind:key="li.data.ad_id"
+      v-for="li in tlist.filter((d, i) => i < transactionCount)"
     >
       <a :href="li.actions.public_view">
-        price: {{ li.data.temp_price }} limits: {{ li.data.min_amount }} ..
-        {{ li.data.max_amount }} | {{ li.data.profile.name }}
+        price: {{ li.data.temp_price }} EUR/BTC limits:
+        {{ li.data.min_amount }} .. {{ li.data.max_amount }} |
+        {{ li.data.profile.name }}
       </a>
     </li>
   </ul>
 </template>
 
 <script>
+// this component renders the items in tlist-prop as a ul-html element.
+// list item count is determined by transactionCount-prop
+
 export default {
   name: 'AdvertisementList',
   props: ['tlist', 'transactionCount'],
