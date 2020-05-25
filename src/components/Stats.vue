d01b60f6e0e91f5486f729465a40f81dabd4fdc8
juissi999 <jussi.kaatiala@gmail.com>
2020-05-25 11:08:28 +0300

    Moved Stats watchers to computed view methods

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,10 +2,10 @@
   <div v-if="recent.length > 0">
     <p>
       Mean price of last 500 transactions:
-      {{ Math.floor(averagePrice(recent) * 100) / 100 }} EUR/BTC
+      {{ Math.floor(averagePrice * 100) / 100 }} EUR/BTC
     </p>
-    <p>Smallest trade: {{ smallest(amounts(recent)) }} BTC</p>
-    <p>Biggest trade: {{ biggest(amounts(recent)) }} BTC</p>
+    <p>Smallest trade: {{ smallest }} BTC</p>
+    <p>Biggest trade: {{ biggest }} BTC</p>
   </div>
 </template>
 
@@ -13,21 +13,31 @@
 export default {
   name: 'Stats',
   props: ['recent'],
-  methods: {
+  computed: {
+    // here define "views" for our data (recent prop)
+
     // calculate average price from array
-    averagePrice: datalist =>
-      datalist.reduce(
+    averagePrice: function() {
+      return this.recent.reduce(
         (accumulator, currentVal) => accumulator + parseFloat(currentVal.price),
         0
-      ) / datalist.length,
+      ) / this.recent.length
+    },
 
     // filter data to get only float prices
-    amounts: datalist => datalist.map(d => parseFloat(d.amount)),
+    amounts: function() {
+      return this.recent.map(d => parseFloat(d.amount))
+    },
 
-    // get smallest and biggest trades
-    smallest: datalist =>
-      datalist.reduce((acc, curVal) => Math.min(acc, curVal)),
-    biggest: datalist => datalist.reduce((acc, curVal) => Math.max(acc, curVal))
+    // get smallest trades
+    smallest: function() {
+      return this.amounts.reduce((acc, curVal) => Math.min(acc, curVal))
+    },
+
+    // get biggest trades
+    biggest: function() {
+      return this.amounts.reduce((acc, curVal) => Math.max(acc, curVal))
+    }
   }
 }
 </script>
