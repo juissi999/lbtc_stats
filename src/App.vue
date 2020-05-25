da8a23fd746d136ab7c47a4a95c9ad19ead14186
juissi999 <jussi.kaatiala@gmail.com>
2020-05-25 12:03:37 +0300

    Add error handling on App

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,18 @@
       {{ TRANSACTION_COUNT }} latest ads
     </h4>
     <h5>Stats (updated {{ statsUpdate }})</h5>
-    <Stats :recent="recentTrades" />
-    <h5>Buy bitcoins ads: (updated {{ this.sellUpdate }})</h5>
-    <p>
+    <p v-if="!errorTrades"><Stats :recent="recentTrades" /></p>
+    <p v-else>Data could not be retrieved</p>
+    <h5>Buy bitcoins ads (updated {{ this.sellUpdate }})</h5>
+    <p v-if="!errorBuys">
       <AdvertisementList :tlist="buys" :transactionCount="TRANSACTION_COUNT" />
     </p>
-    <h5>Sell bitcoins ads: (updated {{ this.sellUpdate }})</h5>
-    <p>
-      <AdvertisementList :tlist="sells" :transactionCount="TRANSACTION_COUNT" />
+    <p v-else>Data could not be retrieved</p>
+    <h5>Sell bitcoins ads (updated {{ this.sellUpdate }})</h5>
+    <p v-if="!errorSells">
+    <AdvertisementList :tlist="sells" :transactionCount="TRANSACTION_COUNT" />
     </p>
+    <p v-else>Data could not be retrieved</p>
   </div>
 </template>
 
@@ -42,7 +45,10 @@ export default {
       buyUpdate: '',
       recentTrades: [],
       sells: [],
-      buys: []
+      buys: [],
+      errorTrades:false,
+      errorBuys: false,
+      errorSells: false
     }
   },
   methods: {
@@ -52,24 +58,33 @@ export default {
       // browser DOM update
 
       // get 500 latest trades
-      lbtcApi.getRecentTrades().then(data => {
-        this.recentTrades = data
-        this.statsUpdate = new Date().toLocaleString()
-      })
+      lbtcApi.getRecentTrades()
+        .then(data => {
+          this.recentTrades = data
+          this.statsUpdate = new Date().toLocaleString()
+          this.errorTrades = false
+        })
+        .catch(err=>this.errorTrades = true)
 
       // get sell advertisements
-      lbtcApi.getSell().then(data => {
-        // sort & place to variable
-        this.sells = data.data.ad_list.sort(descendingSorter)
-        this.sellUpdate = new Date().toLocaleString()
-      })
+      lbtcApi.getSell()
+        .then(data => {
+          // sort & place to variable
+          this.sells = data.data.ad_list.sort(descendingSorter)
+          this.sellUpdate = new Date().toLocaleString()
+          this.errorSells = false
+        })
+        .catch(err=>this.errorSells = true)
 
       // get buy advertisements
-      lbtcApi.getBuy().then(data => {
-        // sort & place to variable
-        this.buys = data.data.ad_list.sort(ascendingSorter)
-        this.buyUpdate = new Date().toLocaleString()
-      })
+      lbtcApi.getBuy()
+        .then(data => {
+          // sort & place to variable
+          this.buys = data.data.ad_list.sort(ascendingSorter)
+          this.buyUpdate = new Date().toLocaleString()
+          this.errorBuys = false
+        })
+        .catch(err=>this.errorBuys = true)
     }
   }
 }
