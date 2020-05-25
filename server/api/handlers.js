30dc24d16c7a1a45e866e1074edc1f3443d991d2
juissi999 <jussi.kaatiala@gmail.com>
2020-05-25 12:02:54 +0300

    Add raw error handling on server proxy

--- server/api/handlers.js
+++ server/api/handlers.js
@@ -5,4 +5,11 @@ exports.proxy = (request, response) => {
   // forward the requests url to baseUrl/...
   const promise = axios.get(`${baseUrl}/${request.url}`)
   promise.then((resp) => response.json(resp.data))
+    .catch((err) => {
+      if (err.response) {
+        response.status(err.response.status).end(err.response.data)
+      } else {
+        response.status(408).end()
+      }
+    })
 }
