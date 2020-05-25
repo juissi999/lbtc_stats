44d82bcf9ce21fad0ffb396cb94c4ba0ab5a07a3
juissi999 <jussi.kaatiala@gmail.com>
2020-05-25 15:44:37 +0300

    Add Transfer component to display single transfer

---
+++ src/components/Transfer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :v-if="transferObj !== null">
+    Latest Transfer {{ new Date(transferObj.date * 1000).toLocaleString() }},
+    {{ transferObj.amount }} BTC / {{ transferObj.price }} EUR
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Transfer',
+  props: ['transferObj']
+}
+</script>
+
+<style></style>
