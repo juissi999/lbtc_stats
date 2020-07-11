a3ea281263f3e1c6e6be979daa1ad8111c3004d4
juissi999 <jussi.kaatiala@gmail.com>
2020-07-11 17:16:02 +0300

    Separate sortedTrades to a computed property

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -46,13 +46,14 @@ export default {
       return this.amounts.reduce((acc, curVal) => Math.max(acc, curVal))
     },
 
+    sortedTrades: function () {
+      return this.recent.sort((a, b) => Number(a.date) - Number(b.date))
+    },
+
     latestTransfer: function () {
-      if (this.recent.length > 1) {
+      if (this.sortedTrades.length > 1) {
         // if the data has some values
-        const sortedTrades = this.recent.sort(
-          (a, b) => Number(a.date) - Number(b.date)
-        )
-        return sortedTrades[sortedTrades.length - 1]
+        return this.sortedTrades[this.sortedTrades.length - 1]
       } else {
         return null
       }
